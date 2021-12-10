de3718c74dbf0e10d9ff6349a8dc30ab2a51122b
renadalkhlafat <renadsalem8888@gmail.com>
2021-12-10 23:26:20 +0200

    feat: AssesmentCategories model added

--- api/models.py
+++ api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Assesment(models.Model):
     assesment_id = models.AutoField(primary_key=True)
-    evaluation = models.ForeignKey('Evaluations', blank=True, null=True,on_delete=models.DO_NOTHING(None,None,None,None))
-    cat = models.ForeignKey('Assesmentcategories', blank=True, null=True,on_delete=models.DO_NOTHING(None,None,None,None))
+    evaluation = models.ForeignKey('Evaluations', blank=True, null=True,on_delete=models.DO_NOTHING)
+    cat = models.ForeignKey('Assesmentcategories', blank=True, null=True,on_delete=models.DO_NOTHING)
     indicator = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
     first_evaluation = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
     final_evaluation = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
@@ -12,3 +12,10 @@ class Assesment(models.Model):
 
     class Meta:
         db_table = 'Assesment'
+
+class Assesmentcategories(models.Model):
+    cat_id = models.AutoField(primary_key=True)
+    category = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
+
+    class Meta:
+        db_table = 'AssesmentCategories'

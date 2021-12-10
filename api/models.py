cd04cedca6c6d1304f7b53454fb9e98201b10c20
renadalkhlafat <renadsalem8888@gmail.com>
2021-12-10 23:18:15 +0200

    feat: Assesment model added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Assesment(models.Model):
+    assesment_id = models.AutoField(primary_key=True)
+    evaluation = models.ForeignKey('Evaluations', blank=True, null=True,on_delete=models.DO_NOTHING(None,None,None,None))
+    cat = models.ForeignKey('Assesmentcategories', blank=True, null=True,on_delete=models.DO_NOTHING(None,None,None,None))
+    indicator = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
+    first_evaluation = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
+    final_evaluation = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
+    score = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'Assesment'

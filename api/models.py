46f56bf071008f62c3d09334ba8c824ce12df6a5
renadalkhlafat <renadsalem8888@gmail.com>
2021-12-11 12:58:15 +0200

    fix: fix database url ,migrated models

--- api/models.py
+++ api/models.py
@@ -6,9 +6,9 @@ class Assesment(models.Model):
     assesment_id = models.AutoField(primary_key=True)
     evaluation = models.ForeignKey('Evaluations', blank=True, null=True,on_delete=models.DO_NOTHING)
     cat = models.ForeignKey('Assesmentcategories', blank=True, null=True,on_delete=models.DO_NOTHING)
-    indicator = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
-    first_evaluation = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
-    final_evaluation = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
+    indicator = models.TextField( blank=True, null=True)
+    first_evaluation = models.TextField( blank=True, null=True)
+    final_evaluation = models.TextField( blank=True, null=True)
     score = models.IntegerField(blank=True, null=True)
 
     class Meta:
@@ -16,7 +16,7 @@ class Assesment(models.Model):
 
 class Assesmentcategories(models.Model):
     cat_id = models.AutoField(primary_key=True)
-    category = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
+    category = models.TextField(blank=True, null=True)
 
     class Meta:
         db_table = 'AssesmentCategories'
@@ -27,8 +27,8 @@ class Evaluations(models.Model):
     user = models.ForeignKey(get_user_model(),on_delete=models.DO_NOTHING)
     evaluated = models.BooleanField()
     academic_year = models.DateTimeField()
-    created_by = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS')
-    status = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS')
+    created_by = models.TextField()
+    status = models.TextField()
     max_score = models.IntegerField()
     score = models.IntegerField(blank=True, null=True)
 
@@ -38,8 +38,8 @@ class Evaluations(models.Model):
 class Goals(models.Model):
     goal_id = models.AutoField(primary_key=True)
     evaluation = models.ForeignKey(Evaluations,on_delete=models.DO_NOTHING)
-    goal = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS')
-    goal_result = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS')
+    goal = models.TextField()
+    goal_result = models.TextField()
     max_score = models.IntegerField()
     score = models.IntegerField()
 
@@ -49,14 +49,14 @@ class Goals(models.Model):
 class Recommendations(models.Model):
     recommender_id = models.AutoField(primary_key=True)
     evaluation = models.ForeignKey(Evaluations,on_delete=models.DO_NOTHING)
-    recommendation = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS')
+    recommendation = models.TextField()
 
     class Meta:
         db_table = 'Recommendations'
 
 # class Roles(models.Model):
 #     role_id = models.AutoField(primary_key=True)
-#     role_name = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS', blank=True, null=True)
+#     role_name = models.TextField( blank=True, null=True)
 
 #     class Meta:
 #         db_table = 'Roles'
@@ -64,7 +64,7 @@ class Recommendations(models.Model):
 class Schools(models.Model):
     school_id = models.AutoField(primary_key=True)
     school_manager = models.ForeignKey(get_user_model(),on_delete=models.DO_NOTHING, db_column='school_manager')
-    school_name = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS')
+    school_name = models.TextField()
     evaluated = models.BooleanField(blank=True, null=True)
     academic_year = models.DateTimeField()
 
@@ -74,7 +74,7 @@ class Schools(models.Model):
 class Subcategories(models.Model):
     sub_cat_id = models.AutoField(primary_key=True)
     cat = models.ForeignKey(Assesmentcategories,on_delete=models.DO_NOTHING)
-    sub_cat = models.TextField(db_collation='SQL_Latin1_General_CP1_CI_AS')
+    sub_cat = models.TextField()
 
     class Meta:
         db_table = 'SubCategories'

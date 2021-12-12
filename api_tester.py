bb473b3db4557625266e4b739adbb331eb7ef17d
renadalkhlafat <renadsalem8888@gmail.com>
2021-12-12 19:05:55 +0200

    test: test get one goal end-point

--- api_tester.py
+++ api_tester.py
@@ -106,6 +106,25 @@ class ApiTester:
 
         return response.json()
 
+
+    def get_one_goal(self):
+        """get list of all resources from api
+        Usage: python api_tester.py get_all
+
+        Returns: JSON
+        """
+        access_token = self.fetch_tokens()[0]
+
+        url = f"{self.host}/goals/1"
+
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+        }
+
+        response = requests.get(url, headers=headers)
+
+        return response.json()
+
 if __name__ == "__main__":
     fire.Fire(ApiTester)
 
